5c07278f964c12aac9785b6368c80c8cfcff50ad
Heliton Pontes <36651605+HelitonPontes@users.noreply.github.com>
2020-02-28 00:41:47 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Pare_de_Cinco_Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            int j = 0;
+            for (i = 1; i < 5; i++)
+            {
+                int n = int.Parse(Console.ReadLine());
+                if (n >= 0)
+                {
+                    j = j + 1;
+                }
+
+            }
+            Console.WriteLine(j + " valores positivos");
+
+            Console.ReadKey();
+        }
+    }
+}
